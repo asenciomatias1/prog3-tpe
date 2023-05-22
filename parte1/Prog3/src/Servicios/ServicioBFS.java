9a2dc6f999a910cdce3b8b808597ddc500103b0a
Matias Asencio <asenciomatias1@gmail.com>
2023-05-22 19:14:05 -0300

    se inicializa el proyecto y se crean las clases y los servicios

---
+++ parte1/Prog3/src/Servicios/ServicioBFS.java
@@ -0,0 +1,21 @@
+package Servicios;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Grafo.Grafo;
+
+public class ServicioBFS {
+
+    private Grafo<?> grafo;
+
+    public ServicioBFS(Grafo<?> grafo) {
+        this.grafo = grafo;
+    }
+
+    public List<Integer> bfsForest() {
+        // Resolver BFS
+        return new ArrayList<>();
+    }
+
+}
---
+++ parte1/Prog3/src/Servicios/ServicioDFS.java
@@ -0,0 +1,21 @@
+package Servicios;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Grafo.Grafo;
+
+public class ServicioDFS {
+
+    private Grafo<?> grafo;
+
+    public ServicioDFS(Grafo<?> grafo) {
+        this.grafo = grafo;
+    }
+
+    public List<Integer> dfsForest() {
+        // Resolver DFS
+        return new ArrayList<>();
+    }
+
+}
